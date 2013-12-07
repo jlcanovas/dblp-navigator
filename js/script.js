72e3e92599f0051c3510f7101284df26029b0aef
jlcanovas <me@jlcanovas.es>
2013-12-07 17:43:50 +0100

    First vresion working with SIGMA.JS

--- js/script.js
+++ js/script.js
@@ -15,15 +15,45 @@ function init() {
         maxRatio: 4
     });
 
-    sigInst.addNode('hello',{
-        label: 'Hello',
+    sigInst.addNode('1',{
+        label: '1',
+        x: Math.random(),
+        y: Math.random(),
         color: '#358753',
         cluster: 'A'
-    }).addNode('world',{
-        label: 'World !',
+    }).addNode('2',{
+        label: '2',
+        x: Math.random(),
+        y: Math.random(),
         color: '#358753',
+        cluster: 'A'
+    }).addNode('3',{
+        label: '3',
+        x: Math.random(),
+        y: Math.random(),
+        color: '#358753',
+        cluster: 'A'
+    }).addNode('4',{
+        label: '4',
+        x: Math.random(),
+        y: Math.random(),
+        color: '#ea7d10',
         cluster: 'B'
-    }).addEdge('hello_world','hello','world').draw();
+    }).addNode('5',{
+        label: '5',
+        x: Math.random(),
+        y: Math.random(),
+        color: '#ea7d10',
+        cluster: 'B'
+    }).addNode('6',{
+        label: '6',
+        x: Math.random(),
+        y: Math.random(),
+        color: '#ea7d10',
+        cluster: 'B'
+    }).addEdge('12','1','2').addEdge('13','1','3').
+       addEdge('45','4','5').addEdge('46','4','6').
+       addEdge('16','1','6').addEdge('26','2','6').draw();
 
     var layoutRunning = false;
     console.log(document.getElementById("layout"));
